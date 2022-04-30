b560f96589d9df9579f265ea333591ac665925cc
fpdsjr <flaviopds.jr@gmail.com>
2022-04-29 22:47:59 -0300

    fix: upload modal

--- photo-galery/photo-galery-frontend/src/components/Upload/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Upload/styles.ts
@@ -1,24 +1,46 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 interface IProps {
-  isDragActive: boolean;
-  isDragReject: boolean;
+  fileLength: number;
 }
 
-const dragActive = css`
-  border-color: #78e5d5;
+export const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 1rem;
+  padding: 1rem 0.5rem;
 `;
 
-const dragReject = css`
-  border-color: #e57878;
-`;
+const dropZoneSizes = {
+  withoutImageWidth: '100%',
+  withoutImageHeight: '780px',
+  withImageWidth: '300px',
+  withImageHeight: '500px',
+};
 
-export const Container = styled.div<IProps>`
-  width: 400px;
-  height: 400px;
-  border: 2px dashed blue;
+const {
+  withImageWidth,
+  withoutImageWidth,
+  withImageHeight,
+  withoutImageHeight,
+} = dropZoneSizes;
+
+export const DropzoneContainer = styled.div<IProps>`
+  padding: 4rem 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  width: ${({ fileLength }) =>
+    fileLength > 0 ? withImageWidth : withoutImageWidth};
+  height: ${({ fileLength }) =>
+    fileLength > 0 ? withImageHeight : withoutImageHeight};
+  border: 2px dashed #d7d7d7;
   border-radius: 0.25rem;
 
-  ${(props) => props.isDragActive && dragActive}
-  ${(props) => props.isDragReject && dragReject}
+  img {
+    object-position: ${({ fileLength }) => (fileLength > 0 ? '-10px' : '')};
+    width: ${({ fileLength }) => (fileLength > 0 ? '280px' : '1200px')};
+    height: ${({ fileLength }) => (fileLength > 0 ? '100%' : '600px')};
+  }
 `;

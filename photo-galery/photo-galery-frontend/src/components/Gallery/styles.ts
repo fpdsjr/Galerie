519417e72aaf6adebb60512d93e027f0d60e2eae
fpdsjr <flaviopds.jr@gmail.com>
2022-04-30 14:13:02 -0300

    feat: add styles to the grid

--- photo-galery/photo-galery-frontend/src/components/Gallery/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Gallery/styles.ts
@@ -3,9 +3,5 @@ import styled from 'styled-components';
 
 export const Container = styled(motion.div)`
   margin-top: 20px;
-  display: flex;
-  flex-flow: row wrap;
-  gap: 1.8rem;
-  align-items: center;
-  justify-content: center;
+  column-count: 4;
 `;

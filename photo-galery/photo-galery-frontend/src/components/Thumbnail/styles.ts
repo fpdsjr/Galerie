676fac1e965bd2c4ceabbeb691cf0f9f4809552b
fpdsjr <flaviopds.jr@gmail.com>
2022-04-30 14:13:20 -0300

    feat: add styles to the images grid

--- photo-galery/photo-galery-frontend/src/components/Thumbnail/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Thumbnail/styles.ts
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 
 export const Image = styled(motion.img)`
   width: 100%;
-  height: 100%;
   z-index: -1;
 `;
 
 export const ImageContainer = styled(motion.div)`
   max-width: 500px;
-  height: 600px;
   overflow: hidden;
+  margin: 0;
+  margin-bottom: 10px;
+  display: flex;
+  break-inside: avoid;
   z-index: -2;
 `;

d0cf04c9d40d6550819585dbf33567e6c44c04f1
fpdsjr <flaviopds.jr@gmail.com>
2022-05-01 13:48:56 -0300

    feat: add animations const

--- photo-galery/photo-galery-frontend/src/components/Thumbnail/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Thumbnail/styles.ts
@@ -1,6 +1,26 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
+export const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
+
+export const thumbnailVariants = {
+  initial: { scale: 0.9, opacity: 0 },
+  enter: { scale: 1, opacity: 1, transition },
+  exit: {
+    scale: 0.5,
+    opacity: 0,
+    transition: { ...transition, duration: 1.5 },
+  },
+};
+
+export const frameVariants = {
+  hover: { scale: 0.95 },
+};
+
+export const imageVariants = {
+  hover: { scale: 1.1 },
+};
+
 export const Image = styled(motion.img)`
   width: 100%;
   z-index: -1;

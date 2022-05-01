df0a7c46f2b719717beb5b974641937e91fe5ab1
fpdsjr <flaviopds.jr@gmail.com>
2022-05-01 13:48:46 -0300

    fix: remove animations const

--- photo-galery/photo-galery-frontend/src/components/Thumbnail/index.tsx
+++ photo-galery/photo-galery-frontend/src/components/Thumbnail/index.tsx
@@ -1,32 +1,19 @@
 import { motion } from 'framer-motion';
 
 import { useImage } from '../../context/ImageInfoProvider/useImage';
-import { Image, ImageContainer } from './styles';
+import {
+  Image,
+  ImageContainer,
+  transition,
+  thumbnailVariants,
+  frameVariants,
+  imageVariants,
+} from './styles';
 
 interface IThumbnailProps {
   handleOpenUploadModal: () => void;
   src: string;
 }
-
-const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
-
-const thumbnailVariants = {
-  initial: { scale: 0.9, opacity: 0 },
-  enter: { scale: 1, opacity: 1, transition },
-  exit: {
-    scale: 0.5,
-    opacity: 0,
-    transition: { ...transition, duration: 1.5 },
-  },
-};
-
-const frameVariants = {
-  hover: { scale: 0.95 },
-};
-
-const imageVariants = {
-  hover: { scale: 1.1 },
-};
 
 function Thumbnail({ handleOpenUploadModal, src }: IThumbnailProps) {
   const ImageInfo = useImage();

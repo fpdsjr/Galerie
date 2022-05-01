8534259d8e54de9b4d0b6352161aa7eeaaaed60c
fpdsjr <flaviopds.jr@gmail.com>
2022-05-01 06:44:44 -0300

    feat: implement the api usage of userdropdown

--- photo-galery/photo-galery-frontend/src/components/UserDropDown/index.tsx
+++ photo-galery/photo-galery-frontend/src/components/UserDropDown/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { useAuth } from '../../context/AuthProvider/useAuth';
+import { Api } from '../../services/api';
 import { Container, DropDownMenuContent, Square } from './styles';
 
 interface IUserDropDown {
@@ -7,13 +9,35 @@ interface IUserDropDown {
   menuRef: React.Ref<HTMLDivElement>;
 }
 
+interface IUserInfo {
+  firstName: string;
+  lastName: string;
+}
+
 function UserDropDown({ modalUser, menuRef }: IUserDropDown) {
+  const [userInfo, setUserInfo] = useState<IUserInfo>({} as IUserInfo);
+  const auth = useAuth();
+
+  async function fetchUserInfo() {
+    const { data } = await Api.get('/user/info');
+
+    setUserInfo(data);
+  }
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+
+  const { firstName, lastName } = userInfo;
+
   return (
     <Container ref={menuRef}>
       {modalUser ? (
         <DropDownMenuContent>
-          <h4>Olá Flavio Junior</h4>
-          <button type="button">Logout</button>
+          <h4>Olá {`${firstName} ${lastName}`}</h4>
+          <button type="button" onClick={auth.logout}>
+            Logout
+          </button>
           <Square />
         </DropDownMenuContent>
       ) : null}

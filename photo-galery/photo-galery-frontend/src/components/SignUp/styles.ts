0678951a4868ef2e05d35b92901c1f0fb70c447d
fpdsjr <flaviopds.jr@gmail.com>
2022-05-01 06:44:15 -0300

    fix: fix buttons signup

--- photo-galery/photo-galery-frontend/src/components/SignUp/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/SignUp/styles.ts
@@ -51,33 +51,16 @@ export const TextContainer = styled.div`
     padding: 0.5rem;
   }
 
-  strong {
+  button {
     cursor: pointer;
     font-weight: 600;
     text-decoration: underline;
     color: #111111;
     transition: all 0.2s;
+    border: 0;
     &:hover {
       transition: all 0.2s ease-in-out 0s;
       color: #cb8805;
-    }
-  }
-
-  button {
-    width: 40rem;
-    height: 2.5rem;
-    font-weight: 700;
-    height: 2.5rem;
-    border-radius: 0.25rem;
-    background: #fec001;
-    color: #273339;
-    border: 1px solid #d7d7d7;
-    text-align: center;
-    transition: all 0.2s;
-
-    &:hover {
-      transition: all 0.2s ease-in-out 0s;
-      filter: brightness(0.8);
     }
   }
 
@@ -88,5 +71,23 @@ export const TextContainer = styled.div`
     input {
       width: 19rem;
     }
+  }
+`;
+
+export const JoinButton = styled.button`
+  width: 40rem;
+  height: 2.5rem;
+  font-weight: 700;
+  height: 2.5rem;
+  border-radius: 0.25rem;
+  background: #fec001;
+  color: #273339;
+  border: 1px solid #d7d7d7;
+  text-align: center;
+  transition: all 0.2s;
+
+  &:hover {
+    transition: all 0.2s ease-in-out 0s;
+    filter: brightness(0.8);
   }
 `;

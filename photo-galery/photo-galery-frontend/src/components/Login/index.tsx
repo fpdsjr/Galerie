e0018c8e910d7f61c114e383a07f52d812256739
fpdsjr <flaviopds.jr@gmail.com>
2022-05-07 15:36:49 -0300

    feat: implement setInvalidUser

--- photo-galery/photo-galery-frontend/src/components/Login/index.tsx
+++ photo-galery/photo-galery-frontend/src/components/Login/index.tsx
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom';
 
 import loginSvgAnimate from '../../assets/login-animation.svg';
 import { useAuth } from '../../context/AuthProvider/useAuth';
+import LoginError from '../LoginError';
 import {
   Container,
+  LoginContainer,
   ImageContainer,
   ButtonContainer,
   JoinButton,
@@ -13,6 +15,8 @@ import {
 function Login() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [invalidUser, setInvalidUser] = useState(false);
+
   const auth = useAuth();
   const navigate = useNavigate();
 
@@ -21,50 +25,52 @@ function Login() {
 
     try {
       await auth.authenticate(email, password);
-      navigate('/home');
-    } catch (err) {
-      console.log(err);
-    }
-  }
 
-  function handleSignUpRedirect() {
-    navigate('/signup');
+      navigate('/home');
+
+      setInvalidUser(false);
+    } catch (err) {
+      setInvalidUser(true);
+    }
   }
 
   const dontHaveAccount = "Don't have an account?";
 
   return (
     <Container>
-      <ImageContainer>
-        <img src={loginSvgAnimate} alt="login" />
-      </ImageContainer>
-      <div>
-        <h1>Login</h1>
-        <p>Welcome Back</p>
-        <label htmlFor="email">
-          <span>Email</span>
-          <input
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label htmlFor="password">
-          <span>Password</span>
-          <input
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <ButtonContainer type="button" onClick={(e) => handleSubmit(e)}>
-          Login
-        </ButtonContainer>
-        <p>
-          {dontHaveAccount}{' '}
-          <JoinButton onClick={() => handleSignUpRedirect()}>Join</JoinButton>
-        </p>
-      </div>
+      {invalidUser && <LoginError invalidUser={invalidUser} />}
+      <LoginContainer>
+        <ImageContainer>
+          <img src={loginSvgAnimate} alt="login" />
+        </ImageContainer>
+        <div>
+          <h1>Login</h1>
+          <p>Welcome Back</p>
+          <label htmlFor="email">
+            <span>Email</span>
+            <input
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor="password">
+            <span>Password</span>
+            <input
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <ButtonContainer type="button" onClick={(e) => handleSubmit(e)}>
+            Login
+          </ButtonContainer>
+          <p>
+            {dontHaveAccount}{' '}
+            <JoinButton onClick={() => navigate('/signup')}>Join</JoinButton>
+          </p>
+        </div>
+      </LoginContainer>
     </Container>
   );
 }

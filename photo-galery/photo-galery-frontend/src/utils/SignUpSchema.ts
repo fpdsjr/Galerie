1aba0d5e6c178481c877b862232e6211f1ea78b8
fpdsjr <flaviopds.jr@gmail.com>
2022-05-07 21:07:36 -0300

    feat: create SignUpSchema

---
+++ photo-galery/photo-galery-frontend/src/utils/SignUpSchema.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const SignUpSchema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});

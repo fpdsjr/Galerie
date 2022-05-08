5df50739a496a2fd7e39d8998e934d914c438f5c
fpdsjr <flaviopds.jr@gmail.com>
2022-05-07 21:09:17 -0300

    refactor: refactor SignUp, remove status status

--- photo-galery/photo-galery-frontend/src/components/SignUp/index.tsx
+++ photo-galery/photo-galery-frontend/src/components/SignUp/index.tsx
@@ -1,21 +1,28 @@
-import React, { SyntheticEvent, useState } from 'react';
+/* eslint-disable react/jsx-no-bind */
+
 import { useNavigate } from 'react-router-dom';
 
 import { useAuth } from '../../context/AuthProvider/useAuth';
-import { Container, ImageContainer, TextContainer, JoinButton } from './styles';
+import SignUpInput from '../SignUpInput';
+import { Container, ImageContainer } from './styles';
+
+interface IValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
 
 function SignUp() {
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
   const auth = useAuth();
   const navigate = useNavigate();
 
-  async function handleSignUp(e: SyntheticEvent) {
-    e.preventDefault();
-
+  async function handleSubmit({
+    firstName,
+    lastName,
+    email,
+    password,
+  }: IValues) {
     try {
       auth.createUser({ firstName, lastName, email, password });
       navigate('/home');
@@ -27,52 +34,7 @@ function SignUp() {
   return (
     <Container>
       <ImageContainer />
-      <TextContainer>
-        <h1>Join Galerie</h1>
-        <div>
-          <label htmlFor="firstName">
-            <span>FirstName</span>
-            <input
-              id="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </label>
-          <label htmlFor="lastName">
-            <span>LastName</span>
-            <input
-              id="lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </label>
-        </div>
-        <label htmlFor="email">
-          <span>Email</span>
-          <input
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label htmlFor="password">
-          <span>Password</span>
-          <input
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <JoinButton type="button" onClick={(e) => handleSignUp(e)}>
-          Join
-        </JoinButton>
-        <p>
-          Already have an account?{' '}
-          <button type="button" onClick={() => navigate('/login')}>
-            Login
-          </button>
-        </p>
-      </TextContainer>
+      <SignUpInput handleSubmit={handleSubmit} />
     </Container>
   );
 }

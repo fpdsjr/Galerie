4fdd21c3da48ee5f7e178cd1f09cfdb834c55104
fpdsjr <flaviopds.jr@gmail.com>
2022-05-08 18:28:01 -0300

    refactor: remove the HeaderGallery

--- photo-galery/photo-galery-frontend/src/components/Header/index.tsx
+++ photo-galery/photo-galery-frontend/src/components/Header/index.tsx
@@ -1,53 +1,15 @@
 import React from 'react';
 
-import usePath from '../../hooks/usePath';
-import {
-  Container,
-  HeaderDefaultButton,
-  MenuImageDropDown,
-  UploadButton,
-} from './styles';
+import { Container, HeaderDefaultButton } from './styles';
 
-interface IHeaderProps {
-  handleOpenUploadModal: () => void;
-  toggleModalUser: () => void;
-  userElement: React.Ref<HTMLDivElement>;
-}
-
-function Header({
-  handleOpenUploadModal,
-  toggleModalUser,
-  userElement,
-}: IHeaderProps) {
-  const { isHome } = usePath();
-
-  function mainHeader() {
-    return (
-      <>
-        <h1>Galerie</h1>
-        <div>
-          <UploadButton type="button" onClick={handleOpenUploadModal}>
-            Upload
-          </UploadButton>
-          <MenuImageDropDown ref={userElement} onClick={toggleModalUser} />
-        </div>
-      </>
-    );
-  }
-
+function Header() {
   return (
     <Container>
-      {isHome ? (
-        mainHeader()
-      ) : (
-        <>
-          <h1>Galerie</h1>
-          <div>
-            <HeaderDefaultButton type="button">About</HeaderDefaultButton>
-            <HeaderDefaultButton type="button">Contact</HeaderDefaultButton>
-          </div>
-        </>
-      )}
+      <h1>Galerie</h1>
+      <div>
+        <HeaderDefaultButton type="button">About</HeaderDefaultButton>
+        <HeaderDefaultButton type="button">Contact</HeaderDefaultButton>
+      </div>
     </Container>
   );
 }
--- photo-galery/photo-galery-frontend/src/components/Header/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Header/styles.ts
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
-// eslint-disable-next-line import/no-unresolved
-import userIcon from '../../assets/userIcon.png';
-
 export const Container = styled.div`
   max-width: 1700px;
   margin: 0 auto;
@@ -36,44 +33,5 @@ export const HeaderDefaultButton = styled.button`
 
   &:hover {
     color: #fec001;
-  }
-`;
-
-export const MenuImageDropDown = styled.div`
-  width: 40px;
-  height: 40px;
-  background-image: url(${userIcon});
-  background-size: 40px;
-  background-repeat: no-repeat;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out 0s;
-
-  &:hover {
-    transform: scale(1.1);
-  }
-`;
-
-export const UploadButton = styled.button`
-  border-radius: 10px;
-  border: none;
-  font-size: 1.1rem;
-  font-weight: 700;
-  width: 140px;
-  background: #fec001;
-  padding: 0 2rem;
-  height: 50px;
-  position: relative;
-  transition: all 0.2s ease-in-out 0s;
-  box-shadow: 1px 1px 5px #384850;
-
-  &:active {
-    background: #f5f5fa;
-  }
-
-  &:hover {
-    box-shadow: 1px 1px 10px black;
-    transition: all 0.2s ease-in-out 0s;
-    transform: scale(1.03);
-    filter: brightness(0.9);
   }
 `;

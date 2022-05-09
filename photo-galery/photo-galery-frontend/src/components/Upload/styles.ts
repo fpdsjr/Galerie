804582a2ffd107bfcff29f64736ff182ab0d72bd
fpdsjr <flaviopds.jr@gmail.com>
2022-05-09 12:38:06 -0300

    refactor: change Upload widthImage

--- photo-galery/photo-galery-frontend/src/components/Upload/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Upload/styles.ts
@@ -7,6 +7,7 @@ interface IProps {
 export const Container = styled.div`
   display: flex;
   flex-flow: row wrap;
+  justify-content: flex-end;
   gap: 1rem;
   padding: 1rem;
 `;
@@ -15,7 +16,7 @@ const dropZoneSizes = {
   withoutImageWidth: '100%',
   withoutImageHeight: '780px',
   withImageWidth: '300px',
-  withImageHeight: '500px',
+  withImageHeight: '710px',
 };
 
 const {
@@ -37,10 +38,46 @@ export const DropzoneContainer = styled.div<IProps>`
     fileLength > 0 ? withImageHeight : withoutImageHeight};
   border: 2px dashed #d7d7d7;
   border-radius: 0.25rem;
+  position: ${({ fileLength }) => (fileLength > 0 ? 'absolute' : '')};
 
   img {
     object-position: ${({ fileLength }) => (fileLength > 0 ? '-10px' : '')};
     width: ${({ fileLength }) => (fileLength > 0 ? '280px' : '1200px')};
     height: ${({ fileLength }) => (fileLength > 0 ? '100%' : '600px')};
   }
+`;
+
+export const DoneButton = styled.button<IProps>`
+  border-radius: 10px;
+  border: none;
+  font-size: 1.1rem;
+  font-weight: 700;
+  width: 300px;
+  background: #fec001;
+  padding: 0 2rem;
+  height: 50px;
+  position: relative;
+  transition: all 0.2s ease-in-out 0s;
+  box-shadow: 1px 1px 5px #384850;
+  position: absolute;
+  bottom: 50px;
+  display: ${({ fileLength }) => (fileLength > 0 ? '1' : 'none')};
+
+  &:active {
+    background: #f5f5fa;
+  }
+
+  &:hover {
+    box-shadow: 1px 1px 10px black;
+    transition: all 0.2s ease-in-out 0s;
+    transform: scale(1.03);
+    filter: brightness(0.9);
+  }
+`;
+
+export const LeftElementContainer = styled.div<IProps>`
+  width: ${({ fileLength }) =>
+    fileLength > 0 ? withImageWidth : withoutImageWidth};
+  height: ${({ fileLength }) =>
+    fileLength > 0 ? withImageHeight : withoutImageHeight};
 `;

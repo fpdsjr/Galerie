72bf1cc53fd2288400bd26d45e473cb5c554150a
fpdsjr <flaviopds.jr@gmail.com>
2022-05-30 13:05:40 -0300

    feat: add responsible login

--- photo-galery/photo-galery-frontend/src/components/Login/styles.ts
+++ photo-galery/photo-galery-frontend/src/components/Login/styles.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  @media (max-width: 1450px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
 
 export const LoginContainer = styled.div`
   max-width: 1700px;
   margin: 0 auto;
   display: flex;
+
+  @media (max-width: 1450px) {
+    flex-direction: column;
+  }
 
   @media (max-width: 450px) {
     width: 100%;
@@ -21,6 +31,13 @@ export const ImageContainer = styled.div`
 
   img {
     width: 50rem;
+  }
+
+  @media (max-width: 1450px) {
+    padding: 0;
+    img {
+      width: 100%;
+    }
   }
 
   @media (max-width: 450px) {
